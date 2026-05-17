ed4ad340318416325dc05f0e544e997dc2803ca2
MaTo <mato157@gmail.com>
2026-05-17 20:21:54 +0200

    Migrate firmware to ESP-IDF Arduino component

---
+++ firmware/RCj_comm_module/main/app_main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+#include "ble.h"
+#include "definitions.h"
+#include "ble_processing.h"
+#include "display.h"
+#include "functions.h"
+#include "state_machine.h"
+
+static void module_setup()
+{
+    Serial.begin(UART_SPEED);
+
+    display_init();
+    module_init_gpios();
+    stm_init();
+    ble_start_server();
+}
+
+static void module_loop()
+{
+    ble_msg_processing();
+    stm_update();
+    check_disconnect_button();
+    check_penalty_button();
+}
+
+extern "C" void app_main(void)
+{
+    initArduino();
+    module_setup();
+
+    while (true) {
+        module_loop();
+        delay(1);
+    }
+}
